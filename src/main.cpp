6839073b7f3c3c7a6269476c6e9b4104d80312c3
Alexey Dmitriev <alex717nd@gmail.com>
2024-11-04 17:05:33 -0800

    Different way of controling Talons that works

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,24 @@
 #include <Arduino.h>
+#include <Servo.h>
 
-// motors, outputs
-const int wheelPWMPin = 6;
-const int forwardChannelPin = 2; // Channel 1
-int speed = 0;
+Servo talonSRX;
+
+const int forwardChannelPin = 2; // Channel that recives signal.  Channel 1
+int forwardSpeed = 0;
 
 // function declarations here:
 int getChannelValue(int);
 
 void setup() {
   Serial.begin(9600);
-  pinMode(wheelPWMPin, OUTPUT);
   pinMode(forwardChannelPin, INPUT);
+  talonSRX.attach(6);
 }
 
 void loop() {
-  speed = getChannelValue(forwardChannelPin);
-  Serial.println(speed);
-  analogWrite(wheelPWMPin, speed);
-  delay(10);
+  forwardSpeed = getChannelValue(forwardChannelPin);
+  Serial.println(forwardSpeed);
+  talonSRX.writeMicroseconds(forwardSpeed);
 }
 
 int getChannelValue(int channelPin) {
@@ -28,19 +28,6 @@ int getChannelValue(int channelPin) {
   }
   channelValue = round(channelValue);
   channelValue = constrain(channelValue, 1000, 2000);
-  channelValue = map(channelValue, 1000, 2000, 0, 255); // constrants channel value to be between 0 and 255
-
-  // if channel value was close to 0 or 255, set channel value to 0 or 255
-  int roundAmount = 5;
-  if (channelValue <= roundAmount) {
-    channelValue = 0;
-  }
-  else if (channelValue >= 255 - roundAmount) {
-    channelValue = 255;
-  }
-  else if (abs(127 - channelValue) <= roundAmount) {
-    channelValue = 127;
-  }
 
   return round(channelValue);
 }
