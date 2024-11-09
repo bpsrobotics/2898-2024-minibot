dca4527552caa979a127b080336228959fa44477
Ant_Throw_Pology <antthrowpology@gmail.com>
2024-11-09 13:26:29 -0800

    channel reads with less blocking time

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # 2898-2024-minibot
 FRC 2898's minibot robot code for the 2024 Bunnybots season
+
+## Channels
+
+**Update this list when you remap the controller configuration**
+
+| Channel | Description                              | Pin  |
+|---------|------------------------------------------|------|
+| 1       | Right stick left/right, left is negative | 2    |
+| 2       | Right stick up/down, down is negative    | 3    |
+| 3       | Left stick up/down, down is negative     | 4    |
+| 4       | Left stick left/right, left is negative  | 5    |
+| 5       | Switch A, up is negative                 | none |
+| 6       | Switch D, up is negative                 | none |
+| 7       | Switch B, up is negative                 | none |
+| 8       | Switch C (3 positions), up is negative   | none |
+| 9       | Variable A, counterclockwise is negative | none |
+| 10      | Variable B, counterclockwise is negative | none |
--- src/main.cpp
+++ src/main.cpp
@@ -3,31 +3,81 @@
 
 Servo talonSRX;
 
-const int forwardChannelPin = 2; // Channel that recives signal.  Channel 1
+// Motor pins
+#define frontLeftMotorPin 6
+#define frontRightMotorPin 9
+#define backLeftMotorPin 10
+#define backRightMotorPin 11
+
+#define rightStickHorizontalPin 2
+#define rightStickVerticalPin 3
+#define leftStickVerticalPin 4
+#define leftStickHorizontalPin 5
+
+#define NOISE_THRESHOLD 100
+
 int forwardSpeed = 0;
 
-// function declarations here:
-int getChannelValue(int);
+struct ChInfo {
+    /** The physical pin the channel is connected to. */
+    uint8_t pin;
+    /** Was the pin `HIGH` last tick? */
+    bool wasOn;
+    /** Did `value` change this tick? */
+    bool valueChanged;
+    /** The last time the pin was detected to be `HIGH`, in microseconds since startup. */
+    unsigned long onUS;
+    /** The value of the channel, from `-1.0` to `1.0`. */
+    float value;
+};
+
+struct {
+    ChInfo rightStickHorizontal = {rightStickHorizontalPin, false, false, 0, 0.0f};
+    ChInfo rightStickVertical = {rightStickVerticalPin, false, false, 0, 0.0f};
+    ChInfo leftStickVertical = {leftStickVerticalPin, false, false, 0, 0.0f};
+    ChInfo leftStickHorizontal = {leftStickHorizontalPin, false, false, 0, 0.0f};
+    // ChInfo channel = {/* pin */2, false, 0};
+} channels;
+
+void updateChannel(ChInfo& channel) {
+    const bool newState = digitalRead(channel.pin);
+    const bool oldState = channel.wasOn;
+    channel.valueChanged = false;
+    if (newState != oldState) {
+        if (newState) {
+            // store rising time
+            channel.onUS = micros();
+            channel.wasOn = true;
+        } else {
+            channel.wasOn = false;
+            // how long was the pulse?
+            unsigned long delta = micros() - channel.onUS;
+            if (delta < NOISE_THRESHOLD) return;
+            channel.value = (float)((long)constrain(delta, 1000, 2000) - 1500) / 500.0f;
+            channel.valueChanged = true;
+        }
+    }
+}
 
 void setup() {
-  Serial.begin(9600);
-  pinMode(forwardChannelPin, INPUT);
-  talonSRX.attach(6);
+    Serial.begin(9600);
+    pinMode(rightStickHorizontalPin, INPUT);
+    pinMode(rightStickVerticalPin, INPUT);
+    pinMode(leftStickVerticalPin, INPUT);
+    pinMode(leftStickHorizontalPin, INPUT);
+    talonSRX.attach(6);
 }
 
 void loop() {
-  forwardSpeed = getChannelValue(forwardChannelPin);
-  Serial.println(forwardSpeed);
-  talonSRX.writeMicroseconds(forwardSpeed);
-}
-
-int getChannelValue(int channelPin) {
-  float channelValue = pulseIn(channelPin, HIGH);
-  if (channelValue < 100) { // not receiving any signal
-    channelValue = 1500; // sets channel value to neutral position
-  }
-  channelValue = round(channelValue);
-  channelValue = constrain(channelValue, 1000, 2000);
-
-  return round(channelValue);
+    updateChannel(channels.rightStickHorizontal);
+    updateChannel(channels.rightStickVertical);
+    updateChannel(channels.leftStickHorizontal);
+    updateChannel(channels.leftStickVertical);
+    // should be interpretable by the Serial Plotter in theory
+    Serial.println(
+        String("RSTICKH:") + channels.rightStickHorizontal.value
+        + ",RSTICKV:" + channels.rightStickVertical.value
+        + ",LSTICKH:" + channels.leftStickHorizontal.value
+        + ",LSTICKV:" + channels.leftStickVertical.value
+    );
 }
