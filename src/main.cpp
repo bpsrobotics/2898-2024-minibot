8e3723e4d966f6cdd890e327582cb606120f236b
Ant_Throw_Pology <antthrowpology@gmail.com>
2024-11-13 20:20:46 -0800

    new input setup?

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 #include <Arduino.h>
-#include <Servo.h>
+#include <ESP32Servo.h>
 #include <Wire.h>
 #include <MPU6050.h>
 #include "mecanum.hpp"
@@ -19,47 +19,51 @@ Servo talonSRX;
 
 #define NOISE_THRESHOLD 100
 
-MPU6050 mpu;
-MecanumDrive drive(0.8, OF_REDUCE_EQUALLY);
+// MPU6050 mpu;
+// MecanumDrive drive(0.8, OF_REDUCE_EQUALLY);
 
 int forwardSpeed = 0;
 
 struct ChInfo {
-    /** The physical pin the channel is connected to. */
+    /* The physical pin the channel is connected to. */
     uint8_t pin;
-    /** Was the pin `HIGH` last tick? */
+    /* Was the pin `HIGH` last time? */
     bool wasOn;
-    /** Did `value` change this tick? */
+    /* Did `value` change this time? */
     bool valueChanged;
-    /** The last time the pin was detected to be `HIGH`, in microseconds since startup. */
+    /* The last time the pin was detected to be `HIGH`, in microseconds since startup. */
     unsigned long onUS;
-    /** The value of the channel, from `-1.0` to `1.0`. */
+    /* The value of the channel, from `-1.0` to `1.0`. */
     float value;
+    /* The minimum and maximum values of the channel, because not all of them are exactly 1000-2000us. */
+    long lowest, highest;
 };
 
 struct {
-    ChInfo rightStickHorizontal = {rightStickHorizontalPin, false, false, 0, 0.0f};
-    ChInfo rightStickVertical = {rightStickVerticalPin, false, false, 0, 0.0f};
-    ChInfo leftStickVertical = {leftStickVerticalPin, false, false, 0, 0.0f};
-    ChInfo leftStickHorizontal = {leftStickHorizontalPin, false, false, 0, 0.0f};
+    // volatile because they're used in interrupts
+    volatile ChInfo rightStickHorizontal = {rightStickHorizontalPin, false, false, 0, 0.0f, 1000, 2000};
+    volatile ChInfo rightStickVertical = {rightStickVerticalPin, false, false, 0, 0.0f, 1000, 2000};
+    volatile ChInfo leftStickVertical = {leftStickVerticalPin, false, false, 0, 0.0f, 1000, 2000};
+    volatile ChInfo leftStickHorizontal = {leftStickHorizontalPin, false, false, 0, 0.0f, 1000, 2000};
     // ChInfo channel = {/* pin */2, false, false, 0, 0.0f};
 } channels;
 
-void updateChannel(ChInfo& channel) {
+void updateChannel(volatile ChInfo& channel) {
     const bool newState = digitalRead(channel.pin);
     const bool oldState = channel.wasOn;
     channel.valueChanged = false;
     if (newState != oldState) {
+        const unsigned long now = micros();
         if (newState) {
             // store rising time
-            channel.onUS = micros();
+            channel.onUS = now;
             channel.wasOn = true;
         } else {
             channel.wasOn = false;
             // how long was the pulse?
-            unsigned long delta = micros() - channel.onUS;
+            long delta = now - channel.onUS;
             if (delta < NOISE_THRESHOLD) return;
-            channel.value = (float)((long)constrain(delta, 1000, 2000) - 1500) / 500.0f;
+            channel.value = (float)(map(constrain(delta, channel.lowest, channel.highest), channel.lowest, channel.highest, 1000, 2000) - 1500) / 500.0f;
             channel.valueChanged = true;
         }
     }
@@ -71,22 +75,22 @@ void setup() {
     pinMode(rightStickVerticalPin, INPUT);
     pinMode(leftStickVerticalPin, INPUT);
     pinMode(leftStickHorizontalPin, INPUT);
-    talonSRX.attach(6);
-    mpu.begin(MPU6050_SCALE_2000DPS, MPU6050_RANGE_16G);
-    mpu.setAccelPowerOnDelay(MPU6050_DELAY_3MS);
-    
-    mpu.setIntFreeFallEnabled(false);
-    mpu.setIntZeroMotionEnabled(false);
-    mpu.setIntMotionEnabled(false);
-    
-    mpu.setDHPFMode(MPU6050_DHPF_5HZ);
+    attachInterrupt(digitalPinToInterrupt(rightStickHorizontalPin), []{ updateChannel(channels.rightStickHorizontal); }, CHANGE);
+    attachInterrupt(digitalPinToInterrupt(rightStickVerticalPin), []{ updateChannel(channels.rightStickVertical); }, CHANGE);
+    attachInterrupt(digitalPinToInterrupt(leftStickVerticalPin), []{ updateChannel(channels.leftStickVertical); }, CHANGE);
+    attachInterrupt(digitalPinToInterrupt(leftStickHorizontalPin), []{ updateChannel(channels.leftStickHorizontal); }, CHANGE);
+    // talonSRX.attach(6);
+    // mpu.begin(MPU6050_SCALE_2000DPS, MPU6050_RANGE_16G);
+    // mpu.setAccelPowerOnDelay(MPU6050_DELAY_3MS);
+    // 
+    // mpu.setIntFreeFallEnabled(false);
+    // mpu.setIntZeroMotionEnabled(false);
+    // mpu.setIntMotionEnabled(false);
+    // 
+    // mpu.setDHPFMode(MPU6050_DHPF_5HZ);
 }
 
 void loop() {
-    updateChannel(channels.rightStickHorizontal);
-    updateChannel(channels.rightStickVertical);
-    updateChannel(channels.leftStickHorizontal);
-    updateChannel(channels.leftStickVertical);
     // should be interpretable by the Serial Plotter in theory
     Serial.println(
         String("RSTICKH:") + channels.rightStickHorizontal.value
